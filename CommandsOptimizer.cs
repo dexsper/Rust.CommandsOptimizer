49ae1a1bb64041c35377ee2a9deb4a79dc3048c3
Sergei Ivanov <dexsperpro@gmail.com>
2024-07-22 03:28:04 +0600

    Update CommandsOptimizer.cs

--- CommandsOptimizer.cs
+++ CommandsOptimizer.cs
@@ -1,84 +1,16 @@
+//Reference: ZString
+
 using System;
 using System.Collections.Generic;
+using Cysharp.Text;
 using HarmonyLib;
 using Network;
 using Oxide.Core;
 using Oxide.Core.Plugins;
-using Oxide.Ext.ZString;
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    public static class CommandBuilder
-    {
-        private static Utf8ValueStringBuilder _sb = ZString.CreateUtf8StringBuilder();
-
-        public static ReadOnlySpan<byte> BuildCommand(ReadOnlySpan<char> command, params object[]? args)
-        {
-            _sb.Clear();
-            _sb.Append(command);
-
-            if (args == null || args.Length == 0)
-                return _sb.AsSpan();
-
-            foreach (object obj in args)
-            {
-                switch (obj)
-                {
-                    case null:
-                        _sb.Append(" \"\"");
-                        break;
-                    case Color color:
-                        _sb.AppendFormat(" \"{0},{1},{2},{3}\"", color.r, color.g, color.b, color.a);
-                        break;
-                    case Vector3 vector:
-                        _sb.AppendFormat(" \"{0},{1},{2}\"", vector.x, vector.y, vector.z);
-                        break;
-                    case IEnumerable<string> strings:
-                    {
-                        foreach (var value in strings)
-                        {
-                            _sb.Append(" ");
-                            QuoteSafe(ref _sb, value);
-                        }
-
-                        break;
-                    }
-                    default:
-                        _sb.Append(" ");
-                        QuoteSafe(ref _sb, obj.ToString());
-                        break;
-                }
-            }
-
-            return _sb.AsSpan();
-        }
-
-        private static void QuoteSafe(ref Utf8ValueStringBuilder builder, string value)
-        {
-            builder.Append('"');
-            int startIndex = 0;
-
-            for (int index = 0; index < value.Length; ++index)
-            {
-                if (value[index] != '"')
-                    continue;
-
-                int count = index - startIndex;
-                if (count > 0)
-                    builder.Append(value.AsSpan(startIndex, count));
-
-                builder.Append("\\\"");
-                startIndex = index + 1;
-            }
-
-            if (startIndex < value.Length)
-                builder.Append(value.AsSpan(startIndex, value.Length - startIndex));
-
-            builder.Append('"');
-        }
-    }
-
     [Info("Commands Optimizer", "Dexsper", "1.0.0")]
     public class CommandsOptimizer : RustPlugin
     {
@@ -146,6 +78,76 @@ namespace Oxide.Plugins
                 }
 
                 return netWrite;
+            }
+        }
+
+        public static class CommandBuilder
+        {
+            private static Utf8ValueStringBuilder _sb = ZString.CreateUtf8StringBuilder();
+
+            public static ReadOnlySpan<byte> BuildCommand(ReadOnlySpan<char> command, params object[]? args)
+            {
+                _sb.Clear();
+                _sb.Append(command);
+
+                if (args == null || args.Length == 0)
+                    return _sb.AsSpan();
+
+                foreach (object obj in args)
+                {
+                    switch (obj)
+                    {
+                        case null:
+                            _sb.Append(" \"\"");
+                            break;
+                        case Color color:
+                            _sb.AppendFormat(" \"{0},{1},{2},{3}\"", color.r, color.g, color.b, color.a);
+                            break;
+                        case Vector3 vector:
+                            _sb.AppendFormat(" \"{0},{1},{2}\"", vector.x, vector.y, vector.z);
+                            break;
+                        case IEnumerable<string> strings:
+                        {
+                            foreach (var value in strings)
+                            {
+                                _sb.Append(" ");
+                                QuoteSafe(ref _sb, value);
+                            }
+
+                            break;
+                        }
+                        default:
+                            _sb.Append(" ");
+                            QuoteSafe(ref _sb, obj.ToString());
+                            break;
+                    }
+                }
+
+                return _sb.AsSpan();
+            }
+
+            private static void QuoteSafe(ref Utf8ValueStringBuilder builder, string value)
+            {
+                builder.Append('"');
+                int startIndex = 0;
+
+                for (int index = 0; index < value.Length; ++index)
+                {
+                    if (value[index] != '"')
+                        continue;
+
+                    int count = index - startIndex;
+                    if (count > 0)
+                        builder.Append(value.AsSpan(startIndex, count));
+
+                    builder.Append("\\\"");
+                    startIndex = index + 1;
+                }
+
+                if (startIndex < value.Length)
+                    builder.Append(value.AsSpan(startIndex, value.Length - startIndex));
+
+                builder.Append('"');
             }
         }
     }
